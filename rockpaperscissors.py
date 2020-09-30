196fc8d3cbcfb78c5f7b9729551771aaf76c8d71
Arnav Mehta <66114536+mehtaarn000@users.noreply.github.com>
2020-09-30 13:49:56 -0500

    Create a user input and win system + grammar fixes
    I created a system that allows users to input their choice of rock, paper, or scissors, and then see if they won against the computer auto-generated choice.

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,26 +1,38 @@
 #User inputs rock paper or scissors
 #Computer generates a choice between the three options
 #User is told if they win or lose
-#Use random.randint to generate a random number. Have 1 = rock, 2 = paper, 3 = sci.
+#Use random.randint to generate a random number. 
+#Have 1 = rock, 2 = paper, 3 = scissors.
 import random
+rock = "The computer got rock."
+paper = "The computer got paper."
+scissors = "The computer got scissors."
+user_query = input("Choose: 'Rock', 'Paper', or 'Scissors'")
+win = "You win!"
+tie = "Tie."
+lose = "You lose."
 
-print("Welcome!!Rock,Paper,or Scissors??")
-
-user = input("What do you say?")
-
-print("Rock,Paper,Scissor ...shoot!")
+print("Rock, Paper, Scissor ...shoot!")
 
 a = random.randint(1, 3)
  
-if a == 1:
-    print("The computer got rock.")
-
-if a == 2:
-    print("The computer got scissors.")
-
-
-if a == 3:
-    print("The computer got paper.")
-
+if a == 1 and user_query == 'Rock':
+    print(rock, tie)
+elif a == 1 and user_query == 'Paper':
+    print(rock, win)
+elif a == 1 and user_query == 'Scissors':
+    print(rock, lose)
+elif a == 2 and user_query == 'Rock':
+    print(paper, lose)
+elif a == 2 and user_query == 'Paper':
+    print(paper, tie)
+elif a == 2 and user_query == 'Scissors':
+    print(paper, win)
+elif a == 3 and user_query == 'Rock':
+    print(scissors, win)
+elif a == 3 and user_query == 'Paper':
+    print(scissors, lose)
+elif a == 3 and user_query == 'Scissors':
+    print(scissors, tie)
 
 print("Thanks for playing and come back soon!!")
